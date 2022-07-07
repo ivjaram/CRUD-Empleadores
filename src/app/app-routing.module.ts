49bcbfc91d82143cfc880ae1fcaaf37b9fcd119c
ivan jara <ivjaram@estudiantesunap.cl>
2022-07-07 16:58:04 -0400

    primer commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'lista', loadChildren: () => import('./view/empleadores/lista/lista.module').then(m => m.ListaModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
